f5dc792c17121feabc63fa36d32145d9fe0574e0
Michee <allidjinoumichee@outlook.fr>
2021-11-21 18:48:31 +0000

    Modify hero

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,12 +1,11 @@
 import Image from 'next/image'
 
-const Hero = ({ children, img, img_alt }) => {
+const Hero = ({ children }) => {
   return (
-    <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center dark:bg-gray-900 text-black dark:text-white w-full py-4 px-2 min-h-screen gap-4">
+    <div className="flex flex-row justify-between items-center dark:bg-gray-900 text-black dark:text-white w-full py-4 px-2 min-h-screen gap-4">
       <div className="flex flex-col gap-4">
       {children}
       </div>
-      {img && <Image src={img} alt={img_alt} />}
     </div>
   )
 }
