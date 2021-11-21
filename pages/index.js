cd3c2c6e413056ba5bfe282f9fc334b9407a12e4
Michee <allidjinoumichee@outlook.fr>
2021-11-21 18:29:18 +0000

    Add project section on homepage

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,23 @@ export default function Home() {
         <p className="text-xl text-red-600 font-bold">Le fab-lab au service de {`l'innovation`}.</p>
         <Button link="/akunalab">En savoir plus</Button>
       </Hero>
+      <Box colReverse="flex-col-reverse" bg="bg-red-500" shadow="shadow-lg">
+        <div className="h-96 w-full relative z-0">
+          <Image
+            src={aboutImg}
+            alt="Picture of soldering"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <div className="flex flex-col gap-4 w-full px-2 py-3 items-end">
+          <h1 className="text-4xl sm:text-4xl font-bold font-mono">Nos Projets</h1>
+          <p className="leading-8 text-right">
+          Nous travaillons sur plusieurs projets avec pour objectif {`d'impacter`} et de transformer notre environnement.
+          </p>
+          <Button link="/projets">Découvrez ce qui se fait à Akuna Lab</Button>
+        </div>
+      </Box>
       <Box bg="bg-yellow-500" shadow="shadow-lg">
         <div className="flex flex-col gap-4 w-full px-2 py-3">
           <h1 className="text-4xl sm:text-4xl font-bold font-mono">Vous avez dit {`"fab lab"`}?</h1>
@@ -53,10 +70,11 @@ export default function Home() {
           <Button link="/akunalab">En apprendre plus sur Akuna</Button>
         </div>
       </Box>
-      <Box colReverse="flex-col-reverse" bg="bg-green-500" shadow="shadow-lg">
+      <Box bg="bg-green-500" shadow="shadow-lg">
         <div className="flex flex-col gap-4 w-full px-2 py-3">
           <h1 className="text-4xl sm:text-4xl font-bold font-mono">Nos équipements</h1>
           <p className="leading-8">
+            Des imprimantes 3d au découpe laser, nous disposons des équipements utilisés aujourd'hui dans le monde de l'innovation.
           </p>
           <Button link="/equipements">Découvrez nos équipements</Button>
         </div>
