7a628e6c8d4fa7aaa19f08149daa53ec76bd2eac
Michee Allidjinou <allidjinoumichee@outlook.fr>
2021-12-19 11:47:17 +0000

    Add page titles

--- pages/equipements.js
+++ pages/equipements.js
@@ -1,14 +1,8 @@
-import Head from 'next/head'
 import Layout from '../components/Layout/Layout'
 
 export default function Equipements() {
   return (
-    <Layout>
-      <Head>
-        <title>Akuna Lab | Equipements</title>
-        <meta name="description" content="Akuna Lab le fab-lab au service de l'innovation" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout pageTitle="Equipements">
       <h1 className="text-4xl font-bold mt-4">Equipements</h1>
     </Layout>
   )
