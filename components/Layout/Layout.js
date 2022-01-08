288ec900631e766925fbffcf6908e40dffda30ef
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-01-08 08:14:16 +0000

    Add pictures

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Footer from "../Footer/Footer"
-import Navbar from "../Navbar/Navbar"
+import Head from "next/head";
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
 
 const Layout = ({ children, pageTitle }) => {
   return (
@@ -8,19 +8,28 @@ const Layout = ({ children, pageTitle }) => {
       <Head>
         <title>{`Akuna Lab | ${pageTitle}`}</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Akuna Lab, le fab lab de l'Institut National Polytechnique Houphouët Boigny de Yamoussoukro!"/>
-        <meta property="og:title" content="Innovons par notre intelligence - Akuna Lab"/>
-        <meta property="og:description" content="Drones, Impression 3D, Photographie, nous disposons de tout pour révolutionner notre environnement."/>
-        <meta property="og:url" content="https://akunalab.netlify.app"/>
-        <meta property="og:type" content="website"/>
+        <meta
+          name="description"
+          content="Akuna Lab, le fab lab de l'Institut National Polytechnique Houphouët Boigny de Yamoussoukro!"
+        />
+        <meta
+          property="og:title"
+          content="Innovons par notre intelligence - Akuna Lab"
+        />
+        <meta
+          property="og:description"
+          content="Drones, Impression 3D, Photographie, nous disposons de tout pour révolutionner notre environnement."
+        />
+        <meta property="og:url" content="https://akunalab.netlify.app" />
+        <meta property="og:type" content="website" />
       </Head>
       <Navbar />
-      <div className="flex flex-col  w-full sm:max-w-5xl mx-auto mb-4 min-h-screen">
+      <div className="flex flex-col gap-4 w-full sm:max-w-5xl mx-auto mb-4 min-h-screen">
         {children}
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
